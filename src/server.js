a1527aec439f454faf0ff9a87b80dc359aafa5e7
kimjaebeom1 <gojn0214@gmail.com>
2022-12-02 22:21:14 +0900

    video call

--- src/server.js
+++ src/server.js
@@ -3,43 +3,11 @@ import { Server } from "socket.io";
 import express from "express";
 
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.render("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
-
-wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg) => console.log(msg));
-});
-
-// const sockets = [];
-
-// // 커넥션이 생겼을 때  socket으로 즉시 hello 메시지를 보냄
-// wss.on("connection", (socket) => {
-//   sockets.push(socket);
-//   socket["nickname"] = "Anonymous";
-//   console.log("Connected to Browser");
-//   socket.on("close", () => console.log("Disconnected from the Browser X"));
-//   socket.on("message", (msg) => {
-//     const messageString = msg.toString("utf8");
-//     const message = JSON.parse(messageString);
-//     switch (message.type) {
-//       case "new_message":
-//         sockets.forEach((aSocket) =>
-//           aSocket.send(`${socket.nickname}: ${message.payload}`)
-//         );
-//         break;
-
-//       case "nickname":
-//         socket["nickname"] = message.payload;
-//     }
-//   });
-// });
-
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-httpServer.listen(3000, handleListen);
